11b89b580c89af4315bae24b8bb509900aab9649
Rahil Mavani <rahil.mavani@mail.bcu.ac.uk>
2026-01-26 20:06:44 +0000

    Task 2 completed

--- Labs/week1/Task2.cpp
+++ Labs/week1/Task2.cpp
@@ -19,18 +19,20 @@ int main()
 	// If you'd like, you can use the setPixel function you wrote in the previous task.
 	// The code below reduces the brightness of the image by 0.5x, as an example.
 
-	/*for(int y = 0; y < height; ++y) 
+	for(int y = 0; y < height; ++y) 
 		for (int x = 0; x < width; ++x) 
 			for (int c = 0; c < 3; ++c) {
-				int pixelIdx = x + y * width;
-				imageBuffer[pixelIdx * 4 + c] *= 0.5;
-			}*/
+				int pixelIdx = (x + y * width) * 4;
+				imageBuffer[pixelIdx + c] = 255 - imageBuffer[pixelIdx + c];
+			}
 
 	// Once you have tested this code, comment out the for loops above and try the following tasks:
 	// * Task 1: Try making a *negative* of the input image. Pixels that are bright in the input
 	//           should be dark in your output. 
 	//           Hint: if the pixels ranged in value from 0 to 1, you could replace each pixel value (v) with (1 - v). 
 	//           but remember, the pixels have 8-bit unsigned values, so range from 0 to 255!
+	
+
 	// * Optional Task 2: Try downsampling this image to 1/2 resolution.
 	//           You can either just keep one in every 4 pixels, or better yet, average the pixels in each 2x2 block.
 	//           Hint: Be careful when averaging! You probably want to convert the pixel values to floating-point to
