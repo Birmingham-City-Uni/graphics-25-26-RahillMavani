f55195c460a78f7f13f932b91817b62e1f40870c
Rahil Mavani <rahil.mavani@mail.bcu.ac.uk>
2026-02-20 23:00:23 +0000

    Week 3 done

--- Labs/week3/Task1.cpp
+++ Labs/week3/Task1.cpp
@@ -33,7 +33,13 @@ void drawTriangle(std::vector<uint8_t>& image, int width, int height,
 	// You can use the std::min and std::max functions if you wish.
 	
 	// YOUR CODE HERE
-	int minX = 0, minY = 0, maxX = 0, maxY = 0;
+
+	int minX, minY, maxX, maxY;
+
+	minX = std::max(0, (int)std::min({ p0.x(), p1.x(), p2.x() }));
+	minY = std::max(0, (int)std::min({ p0.y(), p1.y(), p2.y() }));
+	maxX = std::min(width - 1, (int)std::max({ p0.x(), p1.x(), p2.x() }));
+	maxY = std::min(height - 1, (int)std::max({ p0.y(), p1.y(), p2.y() }));
 
 	// Check your minX, minY, maxX and maxY values don't lie outside the image!
 	// This would cause errors if you attempt to draw there.
@@ -45,7 +51,9 @@ void drawTriangle(std::vector<uint8_t>& image, int width, int height,
 	// from p0 to p1, and from p1 to p2.
 
 	// YOUR CODE HERE
-	Vector2 edge1, edge2;
+	Vector2 edge0 = p1 - p0;
+	Vector2 edge1 = p2 - p1;
+	Vector2 edge2 = p0 - p2;
 
 	// Find the area of the triangle using a cross product.
 	// Optional: You can use the sign of the cross product to see if this triangle is facing towards
@@ -54,7 +62,8 @@ void drawTriangle(std::vector<uint8_t>& image, int width, int height,
 	// for your coursework!)
 
 	// YOUR CODE HERE
-	float triangleArea = 0.0f;
+	float triangleArea = (p1 - p0).cross(p2 - p0);
+	if (std::abs(triangleArea) < 0.000001f) return;
 
 	// Now let's actually draw the triangle!
 	// We'll do a for loop over all pixels in the bounding box.
@@ -67,24 +76,26 @@ void drawTriangle(std::vector<uint8_t>& image, int width, int height,
 
 			// Find the area of each of the three sub-triangles, using a cross product
 			// YOUR CODE HERE - set the value of these three area variables.
-			float a0;
-			float a1;
-			float a2;
+			float a0 = (p2 - p1).cross(p - p1);
+			float a1 = (p0 - p2).cross(p - p2);
+			float a2 = (p1 - p0).cross(p - p0);
 
 			// Find the barycentrics b0, b1, and b2 by dividing by triangle area.
 			// YOUR CODE HERE - do the division and find b0, b1, b2.
-			float b0;
-			float b1;
-			float b2;
+			float b0 = a0 / triangleArea;
+			float b1 = a1 / triangleArea;
+			float b2 = a2 / triangleArea;
 
 			// Check if the sum of b0, b1, b2 is bigger than 1 (or ideally a number just over 1 
 			// to account for numerical error).
 			// If it's bigger, skip to the next pixel as we are outside the triangle.
 			// YOUR CODE HERE
-			float sum;
+			if (b0 >= -0.0001f && b1 >= -0.0001f && b2 >= -0.0001f) {
+				setPixel(image, x, y, width, height, r, g, b, a);
+			}
 
 			// Now we're sure we're inside the triangle, and we can draw this pixel!
-			setPixel(image, x, y, width, height, r, g, b, a);
+			//setPixel(image, x, y, width, height, r, g, b, a);
 		}
 }
 
